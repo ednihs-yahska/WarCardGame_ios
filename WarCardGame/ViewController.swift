eefa3954f463c50e61de051088b940c59400c6ab
Akshay Shinde <ednihs.yahska@gmail.com>
2019-07-12 15:27:22 -0700

    Basic card game

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftCard: UIImageView!
+    
+    @IBOutlet weak var rightCard: UIImageView!
+    
+    @IBOutlet weak var cpuLabel: UILabel!
+    
+    @IBOutlet weak var playerLabel: UILabel!
+    
+    var playerScore = 0
+    
+    var cpuScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftCardNumber = Int.random(in: 2...14)
+        var rightCardNumber = Int.random(in: 2...14)
+        while(leftCardNumber == rightCardNumber){
+            rightCardNumber = Int.random(in: 2...14)
+        }
+        
+        leftCard.image = UIImage(named: "card\(leftCardNumber)")
+        rightCard.image = UIImage(named: "card\(rightCardNumber)")
+        
+        if leftCardNumber > rightCardNumber {
+            playerScore += 1
+            playerLabel.text = "\(playerScore)"
+        } else {
+            cpuScore += 1
+            cpuLabel.text = "\(cpuScore)"
+        }
+    }
+    
 }
 
